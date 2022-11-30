8d745a5e0631b8c36c95d1c259ca5c51c10d7837
AltrexElBellako <ramirez.espejel.aldo@gmail.com>
2022-11-30 04:24:08 -0600

    Se comentó salario.c

--- salario.c
+++ salario.c
@@ -6,37 +6,42 @@
 
 int main()
 {
-	float horas, salaNet, salaNet2, salaNet3, incr, salaBru, reten, reten2;
+//Declaracion de variables para la ejecucion de formulas
+	float horas, costo, salaNet, salaNet2, salaNet3, incr, salaBru, reten, agr;
 	float ivaReten=0.12;
 	float ivaAgr=0.16;
 	float bono=0.05;
-	float costo=100;
-printf("Cálculo de salario\n");
-printf("Ingrese el numero de horas trabajadas: \n");
-scanf("%f",&horas);
-	if (horas<100){
+
+printf("\nCálculo del salario\n\n");
+//Cantidad de horas trabajadas
+	printf("Ingrese el numero de horas trabajadas: \n");
+	scanf("%f",&horas);
+//Costo de las horas de trabajo	
+	printf("Ingrese el pago por hora trabajada \n");
+	scanf("%f",&costo);
+//Formulas para determinar los salarios
 	salaBru = horas*costo;
-	reten = salaBru*ivaReten;
-	salaNet = salaBru-reten;
-	reten2 = salaNet*ivaAgr;
-	salaNet2 = salaNet-reten2;
+	reten = salaNet*ivaReten;
+	salaNet = salaBru+agr;
+	agr = salaBru*ivaAgr;
+	salaNet2 = salaNet-reten;
+//Formula para agregar bono
+	if (salaNet2<10000){
 	incr = salaNet2*bono;
 	salaNet3 = salaNet2+incr;
-printf("Salario neto: %f\n",salaNet3);
-printf("Salario bruto: %f\n",salaNet);
-printf("Retencion del 12 porciento: %f\n",reten);
-printf("Impuesto del 16 porciento: %f\n",reten2);
-printf("Bono del 5 porciento: %f\n",incr);
+//Impresion de valores del salario
+	printf("Salario neto: %f\n",salaNet3);
+	printf("Salario bruto: %f\n",salaBru);
+	printf("Retencion del 12 porciento: %f\n",reten);
+	printf("Impuesto del 16 porciento: %f\n",agr);
+	printf("Bono del 5 porciento: %f\n",incr);
 }
-	else{
-	salaBru= horas*costo;
-	reten= salaBru*ivaReten;
-	salaNet= salaBru-reten;
-	reten2= salaNet*ivaAgr;
-	salaNet2= salaNet-reten2;
-printf("Salario neto: %f\n",salaNet2);
-printf("Salario bruto: %f\n",salaNet);
-printf("Retencion del 12 porciento: %f\n",reten);
-printf("Impuesto del 16 porciento: %f\n",reten2);
+	else
+{
+//Impresion de valores del salario
+	printf("Salario neto: %f\n",salaNet2);
+	printf("Salario bruto: %f\n",salaBru);
+	printf("Retencion del 12 porciento: %f\n",reten);
+	printf("Impuesto del 16 porciento: %f\n",agr);
 }
 }
